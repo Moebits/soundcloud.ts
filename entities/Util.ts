a0b522bc35f27f145d788b12bc408ce7292b02bb
Tenpi <37512637+Tenpi@users.noreply.github.com>
2020-05-31 19:47:15 -0400

    add streamLink

--- entities/Util.ts
+++ entities/Util.ts
@@ -13,6 +13,31 @@ export class Util {
     constructor(private readonly api: api) {}
 
     /**
+     * Gets the direct streaming link of a track.
+     */
+    public streamLink = async (songUrl: string) => {
+        const headers = {
+            "referer": "soundcloud.com",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+        }
+        if (songUrl.includes("m.soundcloud.com")) songUrl = songUrl.replace("m.soundcloud.com", "soundcloud.com")
+        if (!songUrl.includes("soundcloud.com")) songUrl = `https://soundcloud.com/${songUrl}`
+        const html = await axios.get(songUrl, {headers})
+        const match = html.data.match(/(?<=,{"url":")(.*?)(progressive)/)?.[0]
+        let url: string
+        const connect = match.includes("secret_token") ? `&client_id=${this.api.clientID}` : `?client_id=${this.api.clientID}`
+        if (match) {
+            url = await axios.get(match + connect, {headers}).then((r) => r.data.url)
+            .catch(() => {
+                return Promise.reject("client id expired")
+            })
+        } else {
+            return null
+        }
+        return url
+    }
+
+    /**
      * Downloads the mp3 stream of a track.
      */
     public downloadTrackStream = async (songUrl: string, title: string, folder: string) => {
@@ -22,6 +47,7 @@ export class Util {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
         }
         if (songUrl.includes("m.soundcloud.com")) songUrl = songUrl.replace("m.soundcloud.com", "soundcloud.com")
+        if (!songUrl.includes("soundcloud.com")) songUrl = `https://soundcloud.com/${songUrl}`
         const html = await axios.get(songUrl, {headers})
         // const match = html.data.match(/(?<="transcodings":\[{"url":")(.*?)(?=")/)?.[0]
         const match = html.data.match(/(?<=,{"url":")(.*?)(progressive)/)?.[0]
--- start.ts
+++ start.ts
@@ -11,6 +11,7 @@ const soundcloud = new SoundCloud(process.env.SOUNDCLOUD_CLIENT_ID);
     // const result = await soundcloud.util.downloadTrack("https://soundcloud.com/virtual-riot/virtualriot-withyou", "./tracks")
     // const result = await soundcloud.users.getURL("imtenpi")
     // const result = await soundcloud.playlists.searchV2({q: "anime"})
-    const result = await soundcloud.users.getV2("https://soundcloud.com/imtenpi/sets/my-songs")
+    // const result = await soundcloud.users.getV2("https://soundcloud.com/imtenpi/sets/my-songs")
+    const result = await soundcloud.util.streamLink("imtenpi/snowflake")
     console.log(result)
 })()
