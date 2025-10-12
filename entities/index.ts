5c351e4621213f3830946f0338df0674efa6a83a
Tenpi <pie4chan@gmail.com>
2025-10-12 01:35:32 -0400

    remove these broken endpoints

--- entities/index.ts
+++ entities/index.ts
@@ -1,7 +1,4 @@
-export * from "./Apps"
 export * from "./Comments"
-export * from "./Me"
-export * from "./Oembed"
 export * from "./Playlists"
 export * from "./Tracks"
 export * from "./Resolve"
--- soundcloud.ts
+++ soundcloud.ts
@@ -1,5 +1,5 @@
 import {API} from "./API"
-import {Apps, Comments, Me, Oembed, Playlists, Resolve, Tracks, Users, Util} from "./entities"
+import {Comments, Playlists, Resolve, Tracks, Users, Util} from "./entities"
 
 /**
  * The main class for interacting with the Soundcloud API.
@@ -9,10 +9,7 @@ export class Soundcloud {
     public static oauthToken?: string
     public static proxy?: string
     public api = new API(Soundcloud.clientId, Soundcloud.oauthToken)
-    public apps = new Apps(this.api)
     public comments = new Comments(this.api)
-    public me = new Me(this.api)
-    public oembed = new Oembed(this.api)
     public playlists = new Playlists(this.api)
     public resolve = new Resolve(this.api)
     public tracks = new Tracks(this.api)
@@ -25,10 +22,7 @@ export class Soundcloud {
         }
         if (options?.proxy) Soundcloud.proxy = options.proxy
         this.api = new API(Soundcloud.clientId, Soundcloud.oauthToken, Soundcloud.proxy)
-        this.apps = new Apps(this.api)
         this.comments = new Comments(this.api)
-        this.me = new Me(this.api)
-        this.oembed = new Oembed(this.api)
         this.playlists = new Playlists(this.api)
         this.resolve = new Resolve(this.api)
         this.tracks = new Tracks(this.api)
